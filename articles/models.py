12664f446253f1f7663a8056592ad6b04a10967e
PatrickBoynton <jpboynton3@gmail.com>
2021-03-02 11:18:14 -0500

    Added a basic model.

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField(max_length=255)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE)
