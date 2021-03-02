711a745043fb5723e64c69ad7989160da40ac586
PatrickBoynton <jpboynton3@gmail.com>
2021-03-01 21:12:31 -0500

    Removed the form, and moved it to it's own component.

--- frontend/static/src/App.js
+++ frontend/static/src/App.js
@@ -1,21 +1,11 @@
 import './App.css';
 import {Component} from "react";
-
+import Login from './components/Login';
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <form action="">
-                    <h2>Login</h2>
-                    <label htmlFor="username">Username</label>
-                    <input type="text" name="username" id="username"/>
-                    <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="email"/>
-                    <label htmlFor="password">password</label>
-                    <input type="password" name="password" id="password"/>
-                    <button type="submit">Login</button>
-                </form>
-                <p>Don't have an account? Why not <a href="#">Register</a> an account first.</p>
+              <Login />
             </div>
         );
     }
