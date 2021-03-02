31c7cbe3a33e3fefebc7048a0d6a2387b2ee51c0
PatrickBoynton <jpboynton3@gmail.com>
2021-03-02 00:15:24 -0500

    Set up the Register class to accept props.

--- frontend/static/src/components/Register.js
+++ frontend/static/src/components/Register.js
@@ -3,16 +3,32 @@ import { Component } from 'react';
 class Register extends Component{
     render() {
         return <>
-            <form onSubmit={(e) => this.props.handleRegister(e)}>
+            <form onSubmit={(e) => this.props.handleRegister(e, this.props)}>
                 <h2>Register</h2>
                 <label htmlFor="username">Username</label>
-                <input type="text" name="username" id="username"/>
+                <input type="text"
+                       name="username"
+                       onChange={this.props.handleInput}
+                       value={this.props.username}
+                       id="username"/>
                 <label htmlFor="email">Email</label>
-                <input type="email" name="email" id="email"/>
+                <input type="email"
+                       onChange={this.props.handleInput}
+                       value={this.props.email}
+                       name="email"
+                       id="email"/>
                 <label htmlFor="password1">Password</label>
-                <input type="password" name="password1" id="password1"/>
+                <input type="password"
+                       onChange={this.props.handleInput}
+                       value={this.props.password1}
+                       name="password1"
+                       id="password1"/>
                 <label htmlFor="password2">Confirm Password</label>
-                <input type="password" name="password2" id="password2"/>
+                <input type="password"
+                       onChange={this.props.handleInput}
+                       value={this.props.password2}
+                       name="password2"
+                       id="password2"/>
                 <button type="submit">Register</button>
                 <p>Already have an account? Please <a onClick={() => this.props.handleLoginOrRegister()} href="#">Login</a> </p>
             </form>
