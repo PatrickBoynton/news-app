d9c5857d8d6f403e48b372c801c919f576756f4a
PatrickBoynton <jpboynton3@gmail.com>
2021-03-02 00:14:56 -0500

    Set up the login class to accept props.

--- frontend/static/src/components/Login.js
+++ frontend/static/src/components/Login.js
@@ -6,11 +6,23 @@ class Login extends Component {
             <form action="" onSubmit={(e) => this.props.handleLogin(e)}>
                 <h2>Login</h2>
                 <label htmlFor="username">Username</label>
-                <input type="text" name="username" id="username"/>
+                <input type="text"
+                       onChange={this.props.handleInput}
+                       value={this.props.username}
+                       name="username"
+                       id="username"/>
                 <label htmlFor="email">Email</label>
-                <input type="email" name="email" id="email"/>
+                <input type="email"
+                       onChange={this.props.handleInput}
+                       value={this.props.email}
+                       name="email"
+                       id="email"/>
                 <label htmlFor="password">password</label>
-                <input type="password" name="password" id="password"/>
+                <input type="password"
+                       onChange={this.props.handleInput}
+                       value={this.props.password}
+                       name="password"
+                       id="password"/>
                 <button type="submit">Login</button>
             </form>
             <p>Don't have an account? Why not <a onClick={() => this.props.handleLoginOrRegister()} href="#">Register</a> an account first.</p>
