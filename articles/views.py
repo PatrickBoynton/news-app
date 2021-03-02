d0def756999b437117fe90cb5c1a937f4503a4b7
PatrickBoynton <jpboynton3@gmail.com>
2021-03-02 11:22:13 -0500

    Added an articles list view.

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from . import models
+from rest_framework import generics, permissions
+
 
 # Create your views here.
+from .serializers import ArticleSerializer
+
+
+class ArticlesListView(generics.ListAPIView):
+    queryset = models.Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = (permissions.AllowAny, )
