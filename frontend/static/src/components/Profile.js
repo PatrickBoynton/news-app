23b01f56ffb83c21dc3af0abaaedb569099fed82
PatrickBoynton <jpboynton3@gmail.com>
2021-03-02 14:00:32 -0500

    Added a logout method.

--- frontend/static/src/components/Profile.js
+++ frontend/static/src/components/Profile.js
@@ -1,18 +1,36 @@
-import { Component } from 'react'
+import {Component} from 'react';
+import Cookies from 'js-cookie';
 
 class Profile extends Component {
+    constructor(props) {
+        super(props);
+        this.handleLogout = this.handleLogout.bind(this);
+    }
+
+    handleLogout() {
+        const options = {
+            headers: {
+                'Content-Type': 'Application/Json',
+                'X-CSRFToken': Cookies.get('csrftoken')
+            },
+        };
+
+        Cookies.remove("Authorization")
+        this.setState({isLoggedIn: false})
+    }
 
     render() {
         return <>
             <form action="" onSubmit={this.props.handleSubmit}>
-                <input type="file" name="profile_picture" onChange={this.props.handleImage}/>
+                <input className="file" type="file" name="profile_picture" onChange={this.props.handleImage}/>
                 {
                     this.props.profile_picture && <img src={this.props.preview} alt="preview"/>
                 }
-                <button type="submit">Save</button>
+                <button className="form-btn" type="submit">Save</button>
             </form>
-        </>
+            <button onClick={() => this.handleLogout()} className="form-btn">Logout</button>
+        </>;
     }
 }
 
-export default Profile
+export default Profile;
