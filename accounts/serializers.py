0b39b2baaa128e23262791a3577f9245e0c71b09
PatrickBoynton <jpboynton3@gmail.com>
2021-03-02 10:25:41 -0500

    Updated the TokenSerializer.

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
 
+from rest_auth.models import TokenModel
+
 from .models import Profile
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    username = serializers.ReadOnlyField(source='user.username')
+
     class Meta:
         model = Profile
         fields = ('id', 'profile_picture', 'user')
+
+
+class TokenSerializer(serializers.ModelSerializer):
+    username = serializers.ReadOnlyField(source='user.username')
+
+    class Meta:
+        model = TokenModel
+        fields = ('key', 'username')
