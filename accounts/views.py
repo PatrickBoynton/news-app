32e5f3b1a5263334fa123af3a86e4dd544e60591
PatrickBoynton <jpboynton3@gmail.com>
2021-03-02 11:21:48 -0500

    Overrode the default get object and perform create. Added two classes.

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework import generics
-
 from .models import Profile
 from .serializers import ProfileSerializer
 
@@ -9,3 +8,21 @@ from .serializers import ProfileSerializer
 class ProfileListView(generics.ListCreateAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+    # Bases this on the user making the request.
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class ProfileDetailView(generics.RetrieveUpdateAPIView):
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
+
+    # Overrides the get_object method returns the profile or not found error.
+    def get_object(self):
+        return get_object_or_404(Profile, user=self.request.user)
+
+
+class ProfileDetailRetrieveView(generics.RetrieveAPIView):
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
