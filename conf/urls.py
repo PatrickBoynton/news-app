1e7f5f772456ea7850063f21097ad1a2a5554ade
PatrickBoynton <jpboynton3@gmail.com>
2021-03-01 20:52:33 -0500

    Corrected a missing /.

--- conf/urls.py
+++ conf/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
-    path(r'rest-auth', include('rest_auth.urls')),
+    path(r'rest-auth/', include('rest_auth.urls')),
     path(r'rest-auth/registration/', include('rest_auth.registration.urls')),
     path('', include('frontend.urls', namespace='frontend'))
 ]
