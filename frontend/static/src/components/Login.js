8858264ce8a1c8ca195601f847b58ffd000f5b41
PatrickBoynton <jpboynton3@gmail.com>
2021-03-02 13:55:40 -0500

    Moved login to the login class.

--- frontend/static/src/components/Login.js
+++ frontend/static/src/components/Login.js
@@ -1,31 +1,75 @@
 import {Component} from 'react';
+import Cookies from "js-cookie";
 
 class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            email: "",
+            password: ""
+        }
+        this.handleLogin = this.handleLogin.bind(this);
+        this.handleInput = this.handleInput.bind(this);
+    }
+
+    async handleLogin(e, object) {
+        e.preventDefault();
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "Application/Json",
+                "X-CSRFToken": Cookies.get("csrftoken")
+            },
+            body: JSON.stringify({
+                username: object.username,
+                email: object.email,
+                password: object.password
+            }),
+        }
+        const response = await fetch("/rest-auth/login/", options);
+        const data = await response.json().catch(error => console.log(error));
+
+        if (data.key) {
+            Cookies.set("Authorization", `Token ${data.key}`)
+            this.setState({isLoggedIn: true});
+        } else {
+            console.log(data)
+        }
+    }
+
+    handleInput(event) {
+        this.setState({[event.target.name]: event.target.value})
+    }
+
     render() {
         return <>
-            <form action="" onSubmit={(e) => this.props.handleLogin(e, this.props)}>
+            <form action="" onSubmit={(e) => this.handleLogin(e, this.state)}>
                 <h2>Login</h2>
                 <label htmlFor="username">Username</label>
                 <input type="text"
-                       onChange={this.props.handleInput}
-                       value={this.props.username}
+                       onChange={this.handleInput}
+                       value={this.state.username}
                        name="username"
                        id="username"/>
                 <label htmlFor="email">Email</label>
                 <input type="email"
-                       onChange={this.props.handleInput}
-                       value={this.props.email}
+                       onChange={this.handleInput}
+                       value={this.state.email}
                        name="email"
                        id="email"/>
                 <label htmlFor="password">password</label>
                 <input type="password"
-                       onChange={this.props.handleInput}
-                       value={this.props.password}
+                       onChange={this.handleInput}
+                       value={this.state.password}
                        name="password"
                        id="password"/>
-                <button type="submit">Login</button>
+                <button className="form-btn" type="submit">Login</button>
             </form>
-            <p>Don't have an account? Why not <button class="link" onClick={() => this.props.handleLoginOrRegister()}>Register</button> an account first.</p>
+            <p>Don't have an account? Why not <button className="link"
+                                                      onClick={() => this.props.handleLoginOrRegister()}>Register</button> an
+                account first.
+            </p>
         </>
     }
 }
