cb8af37bf2311dcef3405b0cafb51c2e0cd31a60
PatrickBoynton <jpboynton3@gmail.com>
2021-03-03 00:05:51 -0500

    Created an edit url.

--- articles/urls.py
+++ articles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.ArticlesListView.as_view()),
-    path('create/', views.ArticleCreateView.as_view())
+    path('create/', views.ArticleCreateView.as_view()),
+    path('<int:pk>/', views.ArticleEditView.as_view())
 ]
