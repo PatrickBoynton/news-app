cd9be9718be2167c145453888ece9248bba0977d
PatrickBoynton <jpboynton3@gmail.com>
2021-03-03 00:01:23 -0500

    Added a list create.

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render
 from . import models
-from rest_framework import generics, permissions
-
+from rest_framework import generics, permissions, serializers
 
 # Create your views here.
 from .serializers import ArticleSerializer
@@ -10,4 +8,14 @@ from .serializers import ArticleSerializer
 class ArticlesListView(generics.ListAPIView):
     queryset = models.Article.objects.all()
     serializer_class = ArticleSerializer
-    permission_classes = (permissions.AllowAny, )
+    permission_classes = (permissions.AllowAny,)
+
+
+class ArticleCreateView(generics.ListCreateAPIView):
+    queryset = models.Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        user = self.request.user
+        return models.Article.objects.filter(author=user)
