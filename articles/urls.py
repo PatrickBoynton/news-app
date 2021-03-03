b844466dd638c81485bc64e6658d633075caf4ca
PatrickBoynton <jpboynton3@gmail.com>
2021-03-02 23:49:45 -0500

    Added a create url.

--- articles/urls.py
+++ articles/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.ArticlesListView.as_view())
+    path('', views.ArticlesListView.as_view()),
+    path('create/', views.ArticleCreateView.as_view())
 ]
