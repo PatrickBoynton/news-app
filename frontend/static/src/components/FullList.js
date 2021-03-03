56237762fdd6bd4b209f6c24b4de503e593ba626
PatrickBoynton <jpboynton3@gmail.com>
2021-03-03 00:49:01 -0500

    Added a component to display the full list of articles.

---
+++ frontend/static/src/components/FullList.js
@@ -0,0 +1,13 @@
+import {Component} from 'react';
+
+class FullList extends Component{
+    render() {
+        return (
+            <div>
+                Full list of articles
+            </div>
+        );
+    }
+}
+
+export default FullList
