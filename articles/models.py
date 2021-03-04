a04a858f8935075b4e846d972665aa29fe5ac08d
PatrickBoynton <jpboynton3@gmail.com>
2021-03-04 16:08:30 -0500

    Added an article status.

--- articles/models.py
+++ articles/models.py
@@ -12,18 +12,18 @@ class Article(models.Model):
     archived = "archived"
 
     article_choices = [
-        (1, pending),
-        (2, pending),
-        (3, rejected),
-        (4, archived),
+        (pending, pending),
+        (published, published),
+        (rejected, rejected),
+        (archived, archived),
     ]
 
     title = models.CharField(max_length=100)
-    body = models.TextField(max_length=255)
+    body = models.TextField(max_length=2000)
     author = models.ForeignKey(settings.AUTH_USER_MODEL,
                                on_delete=models.CASCADE),
-    article_status = models.CharField(choices=article_choices, max_length=255,
-                                      null=True)
+    article_status = models.CharField(choices=article_choices, max_length=80,
+                                      null=True, blank=True)
 
     def __str__(self):
         return self.title
