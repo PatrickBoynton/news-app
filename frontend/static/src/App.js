f10c034c23e003f547a1f94d2ceefa1c9cd2d178
PatrickBoynton <jpboynton3@gmail.com>
2021-03-04 22:53:01 -0500

    Added props.

--- frontend/static/src/App.js
+++ frontend/static/src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import {Component} from 'react';
 import Header from './components/Header';
-import Register from './components/Register';
-import Login from './components/Login';
-import Profile from './components/Profile';
-import Astronomy from './components/Astronomy';
+import Register from './components/user/Register';
+import Login from './components/user/Login';
+import Profile from './components/user/Profile';
+import Astronomy from './components/news-filters/Astronomy';
 import Cookies from 'js-cookie';
 import {Switch, Route} from 'react-router-dom';
-import FullList from './components/FullList';
-import Cosmology from './components/Cosmology';
-import Exoplanets from './components/Exoplanets';
-import Editorial from './components/Editorial';
+import FullList from './components/news-filters/FullList';
+import Cosmology from './components/news-filters/Cosmology';
+import Exoplanets from './components/news-filters/Exoplanets';
+import Editorial from './components/news-filters/Editorial';
 
 class App extends Component {
     constructor(props) {
@@ -30,19 +30,23 @@ class App extends Component {
         }));
     }
 
+    handleLogin() {
+        this.setState({isLoggedIn: !!Cookies.get('Authorization')});
+    }
+
     render() {
         return (
             <div className="App">
                 <Header isLoggedIn={this.state.isLoggedIn}/>
                 <Switch>
-                        <Route path="/profile" component={Profile}/>
-                        <Route path="/register" component={Register}/>
-                        <Route path="/login" component={Login}/>
-                        <Route path="/astronomy" component={Astronomy}/>
-                        <Route path="/cosmology" component={Cosmology} />
-                        <Route path="/exoplanets" component={Exoplanets}/>
-                        <Route path="/editorial" component={Editorial}/>
-                        <Route path="/" component={FullList}/>
+                    <Route path="/profile" render={(props) => <Profile {...props} isLoggedIn={this.state.isLoggedIn}/>}/>
+                    <Route path="/register" component={Register}/>
+                    <Route path="/login" render={(props) => <Login {...props} isLoggedIn={this.state.isLoggedIn} handleLogin={this.handleLogin}/>}/>
+                    <Route path="/astronomy" component={Astronomy}/>
+                    <Route path="/cosmology" component={Cosmology}/>
+                    <Route path="/exoplanets" component={Exoplanets}/>
+                    <Route path="/editorial" component={Editorial}/>
+                    <Route path="/" component={FullList}/>
                 </Switch>
             </div>
         );
