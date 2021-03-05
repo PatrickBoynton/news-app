97d93dd61e7516b06f3c42b71f419a0a678949a7
PatrickBoynton <jpboynton3@gmail.com>
2021-03-04 22:57:29 -0500

    Working on setting up nav changing on login state.

--- frontend/static/src/components/Header.js
+++ frontend/static/src/components/Header.js
@@ -3,7 +3,9 @@ import {Link} from 'react-router-dom';
 
 class Header extends Component {
     render() {
-        return <header>
+        console.log(this.props.isLoggedIn);
+        if(this.props.isLoggedIn) {
+            return <header>
             <nav>
                 <Link to="/news">News</Link>
                 <Link to="/astronomy">Astronomy</Link>
@@ -11,11 +13,24 @@ class Header extends Component {
                 <Link to="/exoplanets">Exoplanets</Link>
                 <Link to="/editorial">Editorial</Link>
                 <Link to="/profile">Profile</Link>
+                <Link to="/"/>
+            </nav>
+        </header>;
+        } else{
+            return <header>
+            <nav>
+                <Link to="/news">News</Link>
+                <Link to="/astronomy">Astronomy</Link>
+                <Link to="/cosmology">Cosmology</Link>
+                <Link to="/exoplanets">Exoplanets</Link>
+                <Link to="/editorial">Editorial</Link>
                 <Link to="/login">Login</Link>
                 <Link to="/register">Register</Link>
                 <Link to="/"/>
             </nav>
         </header>;
+        }
+
     }
 }
 
