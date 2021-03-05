374d5be1d45df903337d75bdd36c124b8d16a2cf
PatrickBoynton <jpboynton3@gmail.com>
2021-03-05 16:55:58 -0500

    Changed the permissions.

--- articles/views.py
+++ articles/views.py
@@ -5,20 +5,19 @@ from rest_framework import generics, permissions, serializers
 from .serializers import ArticleSerializer
 
 
-class ArticlesListView(generics.ListAPIView):
+class ArticlesListView(generics.ListCreateAPIView):
     queryset = models.Article.objects.all()
     serializer_class = ArticleSerializer
-    permission_classes = (permissions.AllowAny,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 
-class ArticleCreateView(generics.ListCreateAPIView):
-    queryset = models.Article.objects.all()
-    serializer_class = ArticleSerializer
-    permission_classes = (permissions.IsAuthenticated,)
-
-    def perform_create(self):
-        user = self.request.user
-        return models.Article.objects.filter(author=user)
+# class ArticleCreateView(generics.CreateAPIView):
+#     queryset = models.Article.objects.all()
+#     serializer_class = ArticleCreateSerializer
+#     permission_classes = (permissions.IsAuthenticated,)
+#
+#     def perform_create(self, serializer):
+#         serializer.save(author=self.request.user)
 
 
 class ArticleEditView(generics.RetrieveUpdateAPIView):
