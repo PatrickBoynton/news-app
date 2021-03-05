13bfdba0ee45562e0f3048e4365bbae45deed374
PatrickBoynton <jpboynton3@gmail.com>
2021-03-05 16:55:29 -0500

    Commented out the create link.

--- articles/urls.py
+++ articles/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.ArticlesListView.as_view()),
-    path('create/', views.ArticleCreateView.as_view()),
+    # path('create/', views.ArticleCreateView.as_view()),
     path('edit/<int:pk>/', views.ArticleEditView.as_view()),
     path('delete/<int:pk>/', views.ArticleDeleteView.as_view())
 ]
