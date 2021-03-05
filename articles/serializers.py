66bdee1bda7c57e99e4af4a9fefdd5a38276249f
PatrickBoynton <jpboynton3@gmail.com>
2021-03-05 12:47:02 -0500

    Created a new serializer.

--- articles/serializers.py
+++ articles/serializers.py
@@ -3,10 +3,28 @@ from . import models
 
 
 class ArticleSerializer(serializers.ModelSerializer):
+    author = serializers.PrimaryKeyRelatedField(read_only=True)
+
     class Meta:
         model = models.Article
         fields = ('id',
                   'title',
                   'body',
+                  'author',
                   'article_status',
                   'article_type')
+        read_only_fields = ('author',)
+
+
+class ArticleCreateSerializer(serializers.ModelSerializer):
+    author = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = models.Article
+        fields = ('id',
+                  'title',
+                  'body',
+                  'author',
+                  'article_status',
+                  'article_type'
+                  )
