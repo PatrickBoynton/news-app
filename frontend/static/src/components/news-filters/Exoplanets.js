5f72b4823506e7456000cf7131970b831fe10728
PatrickBoynton <jpboynton3@gmail.com>
2021-03-06 16:18:24 -0500

    Hooked up the exo planet section to the api.

--- frontend/static/src/components/news-filters/Exoplanets.js
+++ frontend/static/src/components/news-filters/Exoplanets.js
@@ -1,8 +1,27 @@
 import {Component} from 'react';
 
 class Exoplanets extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            articles: []
+        };
+    }
+
+    async componentDidMount() {
+        const response = await fetch('/api/v1/articles/exoplanets/');
+        const data = await response.json();
+        this.setState({articles: data});
+    }
+
     render() {
-        return <div>Exoplanets</div>;
+        const exo_articles = this.state.articles.map(article => <section key={article.id}>
+            <h2>{article.title}</h2>
+            <p>{article.body}</p>
+            <p>{article.author}</p>
+            <p>{article.article_type}</p>
+        </section>)
+        return <>{exo_articles}</>;
     }
 }
 
