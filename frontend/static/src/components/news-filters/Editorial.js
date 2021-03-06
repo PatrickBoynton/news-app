0b892f165130daf598911c5174dbcce8c4a88dc4
PatrickBoynton <jpboynton3@gmail.com>
2021-03-06 16:13:55 -0500

    Hooked up the editorial section to the api.

--- frontend/static/src/components/news-filters/Editorial.js
+++ frontend/static/src/components/news-filters/Editorial.js
@@ -1,9 +1,29 @@
 import {Component} from 'react';
 
-class Editorial extends Component{
+class Editorial extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            articles: []
+        };
+    }
+
+    async componentDidMount() {
+        const response = await fetch('/api/v1/articles/editorial/');
+        const data = await response.json();
+        this.setState({articles: data});
+    }
+
     render() {
-        return <div>Editorial</div>
+        const editorial_articles = this.state.articles.map(article => <section key={article.id}>
+            <h2>{article.title}</h2>
+            <p>{article.body}</p>
+            <p>{article.author}</p>
+            <p>{article.article_type}</p>
+        </section>)
+        return <>{editorial_articles}</>;
     }
 }
 
-export default Editorial
+export default Editorial;
