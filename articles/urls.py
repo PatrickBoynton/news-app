af6794c46e881d7c29e8c6221c6a42ff639e7e21
PatrickBoynton <jpboynton3@gmail.com>
2021-03-07 23:50:04 -0500

    Fixed a url.

--- articles/urls.py
+++ articles/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.ArticlesListView.as_view()),
-    path('user-articles', views.ArticleDetailView),
-    path('<int:pk>', views.ArticleCreateView.as_view()),
+    path('<int:pk>/', views.ArticleDetailView.as_view()),
+    path('create/', views.ArticleCreateView.as_view()),
     path('<str:article_type>/', views.ArticleFilterView.as_view()),
     path('edit/<int:pk>/', views.ArticleEditView.as_view()),
     path('delete/<int:pk>/', views.ArticleDeleteView.as_view())
