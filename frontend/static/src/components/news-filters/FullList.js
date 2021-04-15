b0f97fc312446370cb1530d945617c047df7b55f
PatrickBoynton <jpboynton3@gmail.com>
2021-04-14 21:46:42 -0400

    Fixed a render issue.

--- frontend/static/src/components/news-filters/FullList.js
+++ frontend/static/src/components/news-filters/FullList.js
@@ -1,43 +1,45 @@
-import {Component} from 'react';
+import { Component } from "react";
 
 class FullList extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            articles: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: [],
     };
-    }
+  }
 
-    async componentDidMount() {
-        const response = await fetch('/api/v1/articles/');
-        const data = await response.json();
-        this.setState({articles: data});
-    }
+  async componentDidMount() {
+    const response = await fetch("/api/v1/articles/");
+    const data = await response.json();
 
+    this.setState({ articles: data });
+    console.log(this.state.articles);
+  }
 
-    render() {
-        const articles = this.state.articles?.map(article => <section key={article.id}>
-            {
-                // Ignore the warnings.
-                article.article_status === 'published'
-                    ?
-                    <>
-                        <h2>{article.title}</h2>
-                        <p>{article.body}</p>
-                        <p>{article.author}</p>
-                        <p>{article.article_type}</p>
-                    </>
-                    :
-                    null
-            }
-        </section>);
-        return (
-            <div>
-                {articles}
-            </div>
-        );
-    }
+  render() {
+    const articles = this.state.articles?.map((article) =>
+      article.article_status === "published" ? (
+        <article key={article.id}>
+          {
+            // Ignore the warnings.
+            <>
+              {article.image ? (
+                <img src={article.image} alt="Nothin' here mate." />
+              ) : null}
+              <h1>
+                <a href="/">{article.title}</a>
+              </h1>
+              <p>{article.body}</p>
+              <p>{article.author}</p>
+              <p>{article.article_type}</p>
+              <p>{article.article_created_at}</p>
+            </>
+          }
+        </article>
+      ) : null
+    );
+    return <div>{articles}</div>;
+  }
 }
 
 export default FullList;
