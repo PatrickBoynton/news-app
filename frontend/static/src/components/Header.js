c7fc80e7ef722045127c10ae896887ff6b0a09dd
PatrickBoynton <jpboynton3@gmail.com>
2021-04-14 21:46:05 -0400

    Formatting.

--- frontend/static/src/components/Header.js
+++ frontend/static/src/components/Header.js
@@ -1,41 +1,59 @@
-import {Component} from 'react';
-import {Link, NavLink} from 'react-router-dom';
+import { Component } from "react";
+import { Link, NavLink } from "react-router-dom";
 
 class Header extends Component {
-    render() {
-        console.log(this.props.isLoggedIn);
-        if(this.props.isLoggedIn) {
-            return <header>
-            <nav>
-                <Link to='/news'>GN</Link>
-                <Link to="/news">News</Link>
-                <Link to="/astronomy">Astronomy</Link>
-                <Link to="/cosmology">Cosmology</Link>
-                <Link to="/exoplanets">Exoplanets</Link>
-                <Link to="/editorial">Editorial</Link>
-                <Link to="/profile">Profile</Link>
-                <Link to="/"/>
-            </nav>
-        </header>;
-        } else{
-            return <header>
-            <nav>
-                <NavLink className='brand' exact to='/'>GN</NavLink>
-                <NavLink activeClassName='active' to="/news">News</NavLink>
-                <NavLink activeClassName='active' to="/astronomy">Astronomy</NavLink>
-                <NavLink activeClassName='active' to="/cosmology">Cosmology</NavLink>
-                <NavLink activeClassName='active' to="/exoplanets">Exoplanets</NavLink>
-                <NavLink activeClassName='active' to="/editorial">Editorial</NavLink>
-                <span style={{paddingLeft: '200px'}} >
-                    <NavLink activeClassName='active' to="/login">Login</NavLink>
-                    <NavLink activeClassName='active' to="/register">Register</NavLink>
-                </span>
-                {/*<NavLink to="/"/>*/}
-            </nav>
-        </header>;
-        }
-
+  render() {
+    console.log(this.props.isLoggedIn);
+    if (this.props.isLoggedIn) {
+      return (
+        <header>
+          <nav>
+            <Link to="/news">GN</Link>
+            <Link to="/news">News</Link>
+            <Link to="/astronomy">Astronomy</Link>
+            <Link to="/cosmology">Cosmology</Link>
+            <Link to="/exoplanets">Exoplanets</Link>
+            <Link to="/editorial">Editorial</Link>
+            <Link to="/profile">Profile</Link>
+            <Link to="/" />
+          </nav>
+        </header>
+      );
+    } else {
+      return (
+        <header>
+          <nav>
+            <NavLink className="brand" exact to="/">
+              GN
+            </NavLink>
+            <NavLink activeClassName="active" to="/news">
+              News
+            </NavLink>
+            <NavLink activeClassName="active" to="/astronomy">
+              Astronomy
+            </NavLink>
+            <NavLink activeClassName="active" to="/cosmology">
+              Cosmology
+            </NavLink>
+            <NavLink activeClassName="active" to="/exoplanets">
+              Exoplanets
+            </NavLink>
+            <NavLink activeClassName="active" to="/editorial">
+              Editorial
+            </NavLink>
+            <span style={{ paddingLeft: "200px" }}>
+              <NavLink activeClassName="active" to="/login">
+                Login
+              </NavLink>
+              <NavLink activeClassName="active" to="/register">
+                Register
+              </NavLink>
+            </span>
+          </nav>
+        </header>
+      );
     }
+  }
 }
 
 export default Header;
