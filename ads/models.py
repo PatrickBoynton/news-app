c0d660bd785b3da829f43657608e6661fba47e44
PatrickBoynton <jpboynton3@gmail.com>
2021-04-16 00:13:51 -0400

    Created an ad model for faux ads.

--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Ad(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    image = models.ImageField(upload_to='ads')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
