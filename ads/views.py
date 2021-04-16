a79d7c4410f5ca3b4e9526ce6c33c4b9f0ae7acf
PatrickBoynton <jpboynton3@gmail.com>
2021-04-16 01:04:59 -0400

    Added a list view and a random view for the ads.

--- ads/views.py
+++ ads/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Ad
+from rest_framework import generics, permissions
+from .serializers import AdSerializer
+
 
 # Create your views here.
+class AdListView(generics.ListAPIView):
+    serializer_class = AdSerializer
+    queryset = Ad.objects.all()
+
+
+class AdRandomView(generics.ListAPIView):
+    serializer_class = AdSerializer
+
+    def get_queryset(self):
+        return Ad.objects.order_by("?")[:1]
