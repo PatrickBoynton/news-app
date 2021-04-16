63a6bd28a3a880287e5d25b357f9e50758ccfed6
PatrickBoynton <jpboynton3@gmail.com>
2021-04-16 00:25:54 -0400

    Removed the body.

--- ads/models.py
+++ ads/models.py
@@ -4,7 +4,6 @@ from django.db import models
 # Create your models here.
 class Ad(models.Model):
     title = models.CharField(max_length=50)
-    body = models.TextField()
     image = models.ImageField(upload_to='ads')
     created_at = models.DateTimeField(auto_now_add=True)
 
