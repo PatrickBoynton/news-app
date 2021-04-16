738c56670968de38cfab2d07b0df4bbcc094853a
PatrickBoynton <jpboynton3@gmail.com>
2021-04-16 14:26:44 -0400

    Formatting, and refactoring, as well as getting rid of errors.

--- frontend/static/src/components/news-filters/FullList.js
+++ frontend/static/src/components/news-filters/FullList.js
@@ -1,30 +1,34 @@
-import { Component } from "react";
-import Article from "./Article";
+import {Component} from 'react';
+import Article from './Article';
+import Ad from '../Ad';
 
 class FullList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: [],
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            articles: [],
+        };
+    }
 
-  async componentDidMount() {
-    const response = await fetch("/api/v1/articles/");
-    const data = await response.json();
+    async componentDidMount() {
+        const response = await fetch('/api/v1/articles/');
+        const data = await response.json();
 
-    this.setState({ articles: data });
-    console.log(this.state.articles);
-  }
+        this.setState({articles: data});
+    }
 
-  render() {
-    const articles = this.state.articles?.map((article) =>
-      article.article_status === "published" ? (
-        <Article article={article} />
-      ) : null
-    );
-    return <div className="container">test {articles} test</div>;
-  }
+    render() {
+        const articles = this.state.articles?.map((article) =>
+            article.article_status === 'published' ? (
+                <Article key={article.id} article={article}/>
+            ) : null
+        );
+        return <div className="container">
+            <Ad/>
+            <div className="item">{articles}</div>
+            <Ad/>
+        </div>;
+    }
 }
 
 export default FullList;
