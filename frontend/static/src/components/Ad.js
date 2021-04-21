0f9585b4343fdab09ee85c88243d4a54e0a26016
PatrickBoynton <jpboynton3@gmail.com>
2021-04-21 10:04:19 -0400

    Changed the permissions.

--- frontend/static/src/components/Ad.js
+++ frontend/static/src/components/Ad.js
@@ -4,18 +4,18 @@ class Ad extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            ad: [],
+            ads: [],
         };
     }
 
     async componentDidMount() {
         const response = await fetch('/api/v1/ads/random/');
         const data = await response.json();
-        this.setState({ad: data});
+        this.setState({ads: data});
     }
 
     render() {
-        const ads = this.state.ad.map(ad => <>
+        const ads = this.state.ads?.map(ad => <>
             <h3><a href="/">{ad.title}</a></h3>
             <img src={ad.image} alt=""/>
             <p>This is a fake ad</p>
